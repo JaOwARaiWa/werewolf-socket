b8407d4826aadf1836fa820ca4d63f5c7391315a
Kiattikun <kiattikun.tho@ku.th>
2023-03-30 07:47:05 +0700

    change PORT in app.js

--- app.js
+++ app.js
@@ -324,5 +324,5 @@ io.on("connection", (socket) => {
     console.log("Client disconnected: " + socket.id);
   });
 });
-const PORT = 3000 || process.env.PORT;
+const PORT = 8081 || process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
